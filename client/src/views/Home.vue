51441b0daae6810faf873e416e75852dba924577
tanner <tcamp022@gmail.com>
2020-05-01 22:20:48 -0400

    test

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Leaving a test message"/>
   </div>
 </template>
 
